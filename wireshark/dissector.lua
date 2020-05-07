66d60b427f2c6fdd209206337d9b61b4f3169d2b
koniczyn5 <koniczyn5@gmail.com>
2020-05-07 12:42:03 +0200

    Dopracowanie komunikatow testowych

--- wireshark/dissector.lua
+++ wireshark/dissector.lua
@@ -6,25 +6,16 @@ cameralink_proto = Proto("cameralink","CameraLink Protocol")
 function cameralink_proto.dissector(buffer,pinfo,tree)
     pinfo.cols.protocol = "CAMERALINK"
     local subtree = tree:add(cameralink_proto,buffer(),"CameraLink Protocol Data")
-    local msg_type = buffer(0,1):bitfield(0, 3)
+    local msg_type = buffer(0,1):bitfield(0, 7)
     
     if msg_type == 1 then 
         subtree:add(buffer(0,1),"TEST_MSG")
         pinfo.cols.info:append(" TEST_MSG")
-        local timeout = buffer(0,4):bitfield(3, 14)
-        local period = buffer(0,4):bitfield(17, 14)
-
-        subtree:add(buffer(0,1),"timeout: " .. timeout)
-        subtree:add(buffer(0,1),"period: " .. period)
-
     elseif msg_type == 2 then 
         subtree:add(buffer(0,1),"TEST_ACK")
         pinfo.cols.info:append(" TEST_ACK")
-        local timeout = buffer(0,4):bitfield(3, 14)
-        local period = buffer(0,4):bitfield(17, 14)
-
-        subtree:add(buffer(0,1),"timeout: " .. timeout)
-        subtree:add(buffer(0,1),"period: " .. period)
+        local testresult = buffer(1,1):bitfield(0, 7)
+	subtree:add(buffer(0,1),"Auto test result: " .. testresult)
     else
         subtree:add(buffer(0,1),"UNKNOWN_MSG " .. msg_type)
         pinfo.cols.info:append(" UNKNOWN_MSG")
