57b0698b0914458c902dbbfa9ab87a6c5207e225
koniczyn5 <koniczyn5@gmail.com>
2020-05-07 15:09:56 +0200

    Dodanie komunikatow konfiguracyjnych

--- wireshark/dissector.lua
+++ wireshark/dissector.lua
@@ -6,7 +6,7 @@ cameralink_proto = Proto("cameralink","CameraLink Protocol")
 function cameralink_proto.dissector(buffer,pinfo,tree)
     pinfo.cols.protocol = "CAMERALINK"
     local subtree = tree:add(cameralink_proto,buffer(),"CameraLink Protocol Data")
-    local msg_type = buffer(0,1):bitfield(0, 7)
+    local msg_type = buffer(0,1):uint()
     
     if msg_type == 1 then 
         subtree:add(buffer(0,1),"TEST_MSG")
@@ -14,8 +14,26 @@ function cameralink_proto.dissector(buffer,pinfo,tree)
     elseif msg_type == 2 then 
         subtree:add(buffer(0,1),"TEST_ACK")
         pinfo.cols.info:append(" TEST_ACK")
-        local testresult = buffer(1,1):bitfield(0, 7)
+        local testresult = buffer(1,1):uint()
 	subtree:add(buffer(0,1),"Auto test result: " .. testresult)
+    elseif msg_type == 3 then
+	subtree:add(buffer(0,1),"CONF_REQ")
+        pinfo.cols.info:append(" CONF_REQ")
+	local rHor = buffer(1,4):uint()
+	local rVer = buffer(5,4):uint()
+	subtree:add(buffer(0,1),"Resolution: ".. rHor .. "x" .. rVer)
+	local focalLength = buffer(9,4):float()
+	subtree:add(buffer(0,1),"focalLength: ".. focalLength)
+	local interval = buffer(13,4):float()
+	subtree:add(buffer(0,1),"Interval: ".. interval)
+	local jitter = buffer(17,4):float()
+	subtree:add(buffer(0,1),"Jitter: ".. jitter)
+    elseif msg_type == 4 then
+        subtree:add(buffer(0,1),"CONF_ACK")
+        pinfo.cols.info:append(" CONF_ACK")
+        local testresult = buffer(1,1):uint()
+	subtree:add(buffer(0,1),"Auto test result: " .. testresult)
+	
     else
         subtree:add(buffer(0,1),"UNKNOWN_MSG " .. msg_type)
         pinfo.cols.info:append(" UNKNOWN_MSG")
