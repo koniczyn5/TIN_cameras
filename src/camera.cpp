5484cc412e150b21084c3f919a7b153d0f9cc504
koniczyn5 <koniczyn5@gmail.com>
2020-05-26 19:29:01 +0200

    zmiana portow

--- src/camera.cpp
+++ src/camera.cpp
@@ -15,9 +15,9 @@
 #define ADDRESS_SIZE 46
 #define TIMEOUT 20
 Camera camera;
-int gatePort4=6666;
-int gatePort6=6667;
-int gatePhotoPort=6668;
+int gatePort4=4001;
+int gatePort6=4002;
+int gatePhotoPort=4003;
 bool isIpv6connected = false;
 bool isIpv4connected = false;
 pthread_cond_t ipv4_cond;
